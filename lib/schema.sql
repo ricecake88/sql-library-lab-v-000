95d52d23f42d59b65766d7082a9ce5def8e16c97
Grace Shih <vocalise@gmail.com>
2018-06-05 04:28:43 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,17 +3,25 @@ CREATE TABLE Characters (
 );
 
 CREATE TABLE Books (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
 );
 
 CREATE TABLE Series (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
 );
 
 CREATE TABLE Authors (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
+  name TEXT
 );
 
 CREATE TABLE Sub-Genres (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
+  name TEXT
 );
